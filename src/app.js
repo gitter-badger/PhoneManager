a44fe9fcc8d857ae67d91008f65ff0247a0cf76c
Abhinav Hardikar <ahardyx@gmail.com>
2016-10-17 08:44:50 +0530

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,29 @@
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
+import TopBar from './components/top-bar'
+import TopTabs from './components/tab-container'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {cyan500} from 'material-ui/styles/colors';
 
-ReactDom.Render(<h1>Hi!</h1>, document.getElementsByTagName('body')[0])
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
+const App = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <div>
+      <TopBar />
+      <TopTabs />
+    </div>
+  </MuiThemeProvider>
+);
+
+document.addEventListener('DOMContentLoaded', function () {
+    ReactDom.render(<App />, document.getElementById('app'))
+});
---
+++ src/components/top-bar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar'
+
+const TopBar = () => (
+    <AppBar title="Phone Manager" showMenuIconButton={false}/>
+);
+
+export default TopBar
