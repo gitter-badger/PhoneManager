eb98c17e85e8708b4b671bf71436840964b45721
Aaron Finch <aaronfinch@poi.us>
2016-10-19 23:01:13 +0200

    TopBar basic style

--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon'
+import {Box, Container} from 'react-layout-components'
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar'
 
+const styles = {
+  toolbar: {
+    backgroundColor: '#87E0E8',
+    color: '#FFF',
+    height: 55,
+    fontFamily: 'roboto, serif',
+    fontSize: 14,
+    paddingLeft: 0,
+    alignItems: 'center',
+    paddingLeft: 212
+  },
+  fontIcon: {
+    color: '#FFF',
+    fontSize: 26,
+  }
+};
 
 const TopBar = () => (
-    <Toolbar>
-      <ToolbarGroup />
-      <ToolbarGroup>
-          <RaisedButton label="Sync" primary={true} />
-      </ToolbarGroup>
+    <Toolbar style={styles.toolbar}>
+      <Box flex="1 1 0" justifyContent="center">
+        <h1 style={styles.title}>Nextbit Robin Phone Manager</h1>
+      </Box>
+      <Box alignContent="flex-end">
+        <FontIcon
+          className="material-icons"
+          style={styles.fontIcon}
+        >
+          info_outline
+        </FontIcon>
+      </Box>
     </Toolbar>
 );
 
